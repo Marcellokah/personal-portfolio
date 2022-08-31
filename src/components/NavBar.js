90aaae59ced71ddf098deaa42aeb9066f16359d7
Marcellokah <aalma47@gmail.com>
2022-08-31 14:09:19 +0200

    Republish repo

---
+++ src/components/NavBar.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import logo from '../assets/img/logo.svg';
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/nav-icon2.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
+
+export const NavBar = () => {
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        }
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+    })
+
+    const onUpdateActiveLink = (value) => {
+        setActiveLink(value);
+    }
+
+    return (
+        <nav className={scrolled ? " navbar navbar-expand-lg scrolled" : "navbar navbar-expand-lg"}>
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">
+                    <img src={logo} alt="Logo" />
+                </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a href="#home" className={activeLink === 'home' ? 'active nav-link navbar-link' : 'nav-link navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</a>
+                        <a href="#skills" className={activeLink === 'skills' ? 'active nav-link navbar-link' : 'nav-link navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</a>
+                        <a href="#projects" className={activeLink === 'projects' ? 'active nav-link navbar-link' : 'nav-link navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</a>
+                    </div>
+                    <span className="navbar-text">
+                        <div className="social-icon">
+                            <a href="#"><img src={navIcon1} alt="" /></a>
+                            <a href="#"><img src={navIcon2} alt="" /></a>
+                            <a href="#"><img src={navIcon3} alt="" /></a>
+                        </div>
+                        <button className="vvd" onClick={() => console.log('connect')}><span>Contact Me</span></button>
+                    </span>
+                </div>
+            </div>
+        </nav>
+    )
+}
---
+++ src/components/Skills.js
@@ -0,0 +1,74 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import htmlLogo from "../assets/img/html.svg";
+import cssLogo from "../assets/img/css.svg";
+import jsLogo from "../assets/img/javascript.svg";
+import reactLogo from "../assets/img/react.svg";
+import boostrapLogo from "../assets/img/bootstrap.svg";
+import threejsLogo from "../assets/img/threejs.svg";
+import colorSharp from "../assets/img/color-sharp.png";
+
+export const Skills = () => {
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
+    return (
+        <section className="skill" id="skills">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="skill-box wow zoomIn">
+                            <h2>
+                                Skills
+                            </h2>
+                            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+                            <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
+                                <div className="item">
+                                    <img src={htmlLogo} alt="HTML" />
+                                    <h5>HTML</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={cssLogo} alt="CSS" />
+                                    <h5>CSS</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={jsLogo} alt="JS" />
+                                    <h5>JS</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={reactLogo} alt="React" />
+                                    <h5>React</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={boostrapLogo} alt="Bootstrap" />
+                                    <h5>Bootstrap</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={threejsLogo} alt="ThreeJS" />
+                                    <h5>ThreeJS</h5>
+                                </div>
+                            </Carousel>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <img className="background-image-left" src={colorSharp} alt="Background"/>
+        </section>
+    )
+}
